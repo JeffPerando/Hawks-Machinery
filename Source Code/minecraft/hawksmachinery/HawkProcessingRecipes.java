af759ecf2f668ec2ec482fefa82000674534e587
Jeff Brusse <be_the_trade@yahoo.com>
2012-07-08 13:35:02 -0700

    Fixed an annoying crash on startup.

--- Source Code/minecraft/hawksmachinery/HawkProcessingRecipes.java
+++ Source Code/minecraft/hawksmachinery/HawkProcessingRecipes.java
@@ -24,7 +24,7 @@ public class HawkProcessingRecipes
 	{
 		UERecipeManager.addRecipe(new ItemStack(mod_HawksMachinery.blockEmptyMachine, 1), new Object[]{"oxo", "x x", "oxo", 'o', BasicComponents.ItemSteelIngot, 'x', BasicComponents.ItemSteelPlate});
 		UERecipeManager.addRecipe((new ItemStack(mod_HawksMachinery.blockProcessor, 1)), new Object[]{"xix", "xpx", "xex", 'x', BasicComponents.ItemSteelIngot, 'p', Item.pickaxeSteel, 'e', mod_HawksMachinery.blockEmptyMachine, 'i', new ItemStack(BasicComponents.ItemCircuit, 1, 1)});
-		UERecipeManager.addShapelessRecipe((new ItemStack(BasicComponents.ItemSteelIngot, 24)), new Object[]{"e", 'e', mod_HawksMachinery.blockEmptyMachine});
+		UERecipeManager.addRecipe((new ItemStack(BasicComponents.ItemSteelIngot, 24)), new Object[]{"e", 'e', mod_HawksMachinery.blockEmptyMachine});
 		
 		UERecipeManager.addSmelting(mod_HawksMachinery.glassDust, new ItemStack(Block.thinGlass));
 		
