90b8e7b976e409ef777d5e4e6245e8b5c2144033
Jeff Brusse <be_the_trade@yahoo.com>
2012-10-22 13:50:00 -0700

    Added a GUI file, will finish later.

---
+++ src/minecraft/hawksmachinery/HMGUIEndiumTeleSender.java
@@ -0,0 +1,22 @@
+
+package hawksmachinery;
+
+import hawksmachinery.tileentity.HMTileEntityTeleporterSender;
+import net.minecraft.src.GuiScreen;
+
+/**
+ * 
+ * 
+ * 
+ * @author Elusivehawk
+ */
+public class HMGUIEndiumTeleSender extends GuiScreen
+{
+	public static HawksMachinery BASEMOD;
+	public HMTileEntityTeleporterSender tileEntity;
+
+	private int containerWidth;
+	private int containerHeight;
+	
+	
+}
