4fe2c7c915ffeb4576578148f46e9892dfec3c4d
Jeff Brusse <be_the_trade@yahoo.com>
2012-10-26 02:31:13 -0700

    Fixed the Rivet Gun's sprite/texture.
    Who's smart ideas was it to separate textures and sprites in the first place?

--- src/common/hawksmachinery/items/HMItemRivetGun.java
+++ src/common/hawksmachinery/items/HMItemRivetGun.java
@@ -29,7 +29,7 @@ public class HMItemRivetGun extends ItemElectric
 		super(id);
 		setTextureFile(BASEMOD.ITEM_TEXTURE_FILE);
 		setItemName("rivetGun");
-		setIconIndex(17);
+		setIconIndex(29);
 		
 	}
 	
