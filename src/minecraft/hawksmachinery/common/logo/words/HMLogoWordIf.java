cf643c8e9dfc10bde16d56022b205e5c688a5538
Jeff Brusse <be_the_trade@yahoo.com>
2012-12-29 02:26:03 -0800

    I almost feel like I've fallen for Schmuck Bait...

---
+++ src/minecraft/hawksmachinery/common/logo/words/HMLogoWordIf.java
@@ -0,0 +1,52 @@
+
+package hawksmachinery.common.logo.words;
+
+import hawksmachinery.common.api.logo.HMLogoError;
+import hawksmachinery.common.api.logo.IHMLogoWord;
+import hawksmachinery.common.api.logo.IHMRobot;
+
+/**
+ * 
+ * 
+ * 
+ * @author Elusivehawk
+ */
+public class HMLogoWordIf implements IHMLogoWord
+{
+	@Override
+	public String getWord()
+	{
+		return "if";
+	}
+	
+	@Override
+	public String getShortWord()
+	{
+		return null;
+	}
+	
+	@Override
+	public boolean supportsNesting()
+	{
+		return true;
+	}
+	
+	@Override
+	public int argCount()
+	{
+		return 0;
+	}
+	
+	@Override
+	public boolean isValidArgument(String arg)
+	{
+		return true;
+	}
+	
+	@Override
+	public HMLogoError activateWord(String[] args, IHMRobot robot)
+	{
+		return null;
+	}
+	
+}
