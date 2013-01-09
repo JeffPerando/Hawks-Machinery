56b24b1228c991d8aa3ca1a0a2118d9dfef75520
Jeff Brusse <be_the_trade@yahoo.com>
2013-01-08 21:55:32 -0800

    Updated UE.

--- src/minecraft/hawksmachinery/common/HMCobaltOreGen.java
+++ src/minecraft/hawksmachinery/common/HMCobaltOreGen.java
@@ -13,13 +13,12 @@ import universalelectricity.prefab.ore.OreGenReplace;
  */
 public class HMCobaltOreGen extends OreGenReplace
 {
-	public static HawksMachinery BASEMOD;
-	
 	public HMCobaltOreGen()
 	{
 		super("Cobalt Ore", "oreCobalt", new ItemStack(HMBlock.ore, 1, 1), 1, 12, 30, 32, 1, "pickaxe", 2);
-		this.generateSurface = true;
-		this.shouldGenerate = BASEMOD.instance().MANAGER.generateCobalt;
+		this.ignoreNether = true;
+		this.ignoreEnd = true;
+		this.shouldGenerate = HawksMachinery.instance().MANAGER.generateCobalt;
 		
 	}
 	
--- src/minecraft/hawksmachinery/common/HMEndiumOreGen.java
+++ src/minecraft/hawksmachinery/common/HMEndiumOreGen.java
@@ -13,13 +13,12 @@ import universalelectricity.prefab.ore.OreGenReplace;
  */
 public class HMEndiumOreGen extends OreGenReplace
 {
-	public static HawksMachinery BASEMOD;
-	
 	public HMEndiumOreGen()
 	{
 		super("Endium Ore", "oreEndium", new ItemStack(HMBlock.ore), 121, 12, 64, 8, 8, "pickaxe", 3);
-		this.generateEnd = true;
-		this.shouldGenerate = BASEMOD.instance().MANAGER.generateEndium;
+		this.ignoreNether = true;
+		this.ignoreSurface = true;
+		this.shouldGenerate = HawksMachinery.instance().MANAGER.generateEndium;
 		
 	}
 	
